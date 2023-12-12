aa6ac9f37e1886e34a92ec3c477db46acc76133a
Vladimir Afonin <vovan010698@mail.ru>
2023-12-13 00:27:38 +0300

    commit bigword and title_str

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
 def big_word(word):
+    """функция делает все буквы заглавными"""
     return word.upper()
 
+def title_str(str):
+    """функция делает первую букву заглавной"""
+    return str.title()
